4bd38a69767bbb947bcf0de6928ed119e7c3eb68
jagadeeshbala29 <103747260+jagadeeshbala29@users.noreply.github.com>
2025-11-15 19:44:13 +0530

    initial 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (  
+      <div>
+      <h1 className="text-3xl font-bold text-blue-600">
+    Hello world!
+  </h1>
+      </div>
+  )
+}
+
+export default App
